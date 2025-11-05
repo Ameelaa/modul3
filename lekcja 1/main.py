ca3a99c0ce7059cb13b389193c27ebaaa3e93cb5
Ameelaa <amelianatalia.komor@gmail.com>
2025-11-05 20:23:49 +0100

    first commit

---
+++ lekcja 1/main.py
@@ -0,0 +1,25 @@
+
+from flask import Flask
+import random
+
+app = Flask(__name__)
+
+
+
+@app.route("/przywitanie")
+def przywitanie():
+    return """<h1>Hello</h1>
+    <a href="/ciekawostka"> ciekawostka!</a>"""
+
+@app.route("/ciekawostka")
+def ciekawostka():
+    facts_list = ["Większość osób cierpiących na uzależnienie technologiczne doświadcza silnego stresu, gdy znajdują się poza zasięgiem sieci lub nie mogą korzystać ze swoich urządzeń.",
+                  "Według badania przeprowadzonego w 2018 roku ponad 50% osób w wieku od 18 do 34 lat uważa się za zależne od swoich smartfonów.",
+                  "Badanie zależności technologicznych jest jednym z najważniejszych obszarów współczesnych badań naukowych.",
+                  "Według badania z 2019 r. ponad 60% osób odpowiada na wiadomości służbowe na swoich smartfonach w ciągu 15 minut po wyjściu z pracy."]
+
+    return f"""<p>{random.choice(facts_list)}</p>
+<a href="/przywitanie"> strona główna</a>"""
+
+app.run(debug=True)
+
